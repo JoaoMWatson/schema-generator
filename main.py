9fdd3260dfa531a70b706125a137d82f27915170
JoaoMWatson <joaopedrowatson@hotmail.com>
2021-10-30 22:55:44 -0300

    Criação node schema

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import typer
 import pandas as pd
 from rich.console import Console
+from src.schema import SchemaBody
+
 
 app = typer.Typer()
 console = Console()
@@ -11,16 +13,22 @@ cur_path = os.path.dirname(__file__)
                                                                                  
 
 @app.command()
-def main(file: str = typer.Argument(..., help="File to parse"), sheet_number: int = typer.Argument(..., help="Sheet number to parse")):
+def main(file: str = typer.Argument(..., help="File to parse"), sheet_number: int = typer.Argument(..., help="Sheet number to parse"), topic_name: str = typer.Argument(..., help="Topic name to parse")):
     """
+    Parse a CSV file and create a topic in the specified topic name.
     """
     try:
         new_path: str = os.path.relpath(file, cur_path)
         
         dataFrame: pd.DataFrame = pd.read_excel(new_path, sheet_name=sheet_number, header=19, usecols="B:D, F, H").dropna()
-
-        console.log(dataFrame)
         
+        console.log(dataFrame.to_dict())
+        schema_body = SchemaBody(dataFrame)
+        fields = []
+        for index, row in dataFrame.iterrows():
+            fields.append(schema_body.create_node(row["Tipo"], row["Campo"], row["Tamanho"], row["Aceita"]))
+        
+        console.log(fields)
     except IOError:
         console.print("[bold red]File not found[/bold red]")
         
--- src/schema.py
+++ src/schema.py
@@ -2,8 +2,6 @@ from typing import Dict
 
 
 class SchemaBody:
-    def __init__(self, fields):
-        self.fields = fields
 
     def __str__(self):
         return self.name + "(" + str(self.fields) + ")"
@@ -11,14 +9,43 @@ class SchemaBody:
     def __repr__(self):
         return self.name + "(" + str(self.fields) + ")"
 
-    def _set_type(self, type: str):
-        pass
+    def _set_type(self, typeof: str = 'string', name: str = None, size: str = None):
+        try:
+            if typeof == 'string':
+                seted_type = "string"
+            elif typeof == 'fixed':
+                seted_type = {
+                    "name": name,
+                    "size": size,
+                    "type": typeof
+                }
+            elif typeof == 'float':
+                seted_type = {
+                    "connect.name": "org.apache.kafka.connect.data.Timestamp",
+                    "connect.version": 1,
+                    "logicalType": "timestamp-millis",
+                    "type": "long"
+                }
+            else:
+                raise f"Unknown Type Error: {typeof}"
+            return seted_type
+        except Exception as e:
+            print(e)
 
-    def create_node(self):
-        is_nullable: bool = True if 'Sim' in self.fields['Aceita Nulo'] else False
-        node = {
-                "doc": self.fields['Comentários'],
-                "name": self.fields['Campo'],
-                "type": self._set_type(self.fields['Tipo']),
+    def create_node(self, typeof: str = 'string', name: str = None, size: str = None, is_null: str = None, comentarios: str = None):
+        is_nullable: bool = True if 'Sim' in is_null else False
+        node = {}
+        if is_nullable:
+            node = {
+                "default": "null",
+                "doc": comentarios,
+                "name": name,
+                "type": ["null", self._set_type(typeof, size=size, name=name)],
+            }
+        else:
+            node = {
+                "doc": comentarios,
+                "name": name,
+                "type": self._set_type(typeof, size=size, name=name),
             }
         return node
